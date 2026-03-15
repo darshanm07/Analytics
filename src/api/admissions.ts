539fa888f659ce8eabaf9ed102590a8ccd84d014
darshansagar7 <mungalparadarshan1@gmail.com>
2026-03-15 15:53:57 +0530

    change api key

--- src/api/admissions.ts
+++ src/api/admissions.ts
@@ -2,9 +2,9 @@ import type { AdmissionsData } from "../types";
 
 export const fetchAdmissions = async (): Promise<AdmissionsData> => {
   const res = await fetch(
-    "https://mocki.io/v1/cc94fb19-66de-4881-9cb3-6e57a5a475c5"
+    "https://mocki.io/v1/ceff4e01-67e7-49b7-b77b-360f65e08a49",
   );
   if (!res.ok) throw new Error("Failed to fetch admissions data");
   const data = await res.json();
-  return data.admissions;
+  return data;
 };
